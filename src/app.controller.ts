079d9b231ff0c19373c30f6732252369c8ed4189
IssacGo <g9191hc@gmail.com>
2023-11-27 17:39:30 +0900

    포스트 JSON 반환하기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/**
+ * author: string;
+ * title: string;
+ * content: string;
+ * likeCount: number;
+ * commentCount: number;
+ */
+
+interface Post{
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'Issac Go',
+      title: 'Practice',
+      content: 'Practice content',
+      likeCount: 10000,
+      commentCount: 9999
+    };
   }
 }
